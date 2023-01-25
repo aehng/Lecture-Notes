ac916b93623b4103d9650fc65632a67fcf19dc33
Erik on Endeavour <erik.falor@usu.edu>
2023-01-25 15:27:11 -0700

    lecture result 1/25

---
+++ Module1/Lec07-Wed_Jan_25/main.py
@@ -0,0 +1,16 @@
+from idlelib.multicall import r
+from self import self
+
+
+class main:
+    def __init__(self):
+        self.state = "I'm a little confused..."
+
+    def main(self):
+        print(r)
+        print(n)
+        print(q)
+        print(self.naem)
+
+    if __name__ == "__main__":
+        main(self)
